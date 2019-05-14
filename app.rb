0314d30933e367e14adf5cb7d6620742b9ec8416
daglus <daglus.ck.ua@gmail.com>
2019-05-14 20:15:42 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  #class method
   def self.say_foo
     puts "Fooo!"
+  end
+
+  #instance method
+  def log_foo bar
+    @f.puts bar
   end
 end
 
 Logger.say_foo
+
+logger = Logger.new
+logger.log_foo 'Wow!'
